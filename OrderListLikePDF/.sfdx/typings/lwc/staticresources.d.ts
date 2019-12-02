8fde64476d042f30e5b1632bd3309b8d92f3b055
SvetaTestProject <svetlanalevaniuk@gmail.com>
2019-12-02 14:20:34 +0300

    added page and controller for rendering as PDF,  added component, simple changes to the orderList page and its controller

---
+++ OrderListLikePDF/.sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,36 @@
+declare module "@salesforce/resourceUrl/Aframax" {
+    var Aframax: string;
+    export default Aframax;
+}
+declare module "@salesforce/resourceUrl/CoverWagon" {
+    var CoverWagon: string;
+    export default CoverWagon;
+}
+declare module "@salesforce/resourceUrl/Elf" {
+    var Elf: string;
+    export default Elf;
+}
+declare module "@salesforce/resourceUrl/Handysize" {
+    var Handysize: string;
+    export default Handysize;
+}
+declare module "@salesforce/resourceUrl/LetL410" {
+    var LetL410: string;
+    export default LetL410;
+}
+declare module "@salesforce/resourceUrl/PeugeotBOXER" {
+    var PeugeotBOXER: string;
+    export default PeugeotBOXER;
+}
+declare module "@salesforce/resourceUrl/Platforms11066" {
+    var Platforms11066: string;
+    export default Platforms11066;
+}
+declare module "@salesforce/resourceUrl/Saab340" {
+    var Saab340: string;
+    export default Saab340;
+}
+declare module "@salesforce/resourceUrl/emailLog" {
+    var emailLog: string;
+    export default emailLog;
+}
---
+++ OrderListLikePDF/force-app/main/default/triggers/DeliveryOrderTrigger.trigger
@@ -0,0 +1,14 @@
+trigger DeliveryOrderTrigger on DeliveryOrder__c (before insert, before update, after insert) {
+    
+    if (Trigger.isBefore){        
+        if (Trigger.isInsert){
+            DeliveryOrderTriggerHelper.handlerBeforeInsert(Trigger.new);
+        }
+        if (Trigger.isUpdate){
+            DeliveryOrderTriggerHelper.handlerBeforeUpdate(Trigger.new, Trigger.old);
+        }
+    }
+  /*  if (Trigger.isAfter) {
+        DeliveryOrderTriggerHelper.handlerAfterInsert(Trigger.new);
+    }*/
+}
